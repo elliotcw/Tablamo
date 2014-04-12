fc3faa1fa369cfd94fbfe0542794d68305a70709
elliotcw <williams.elliot@gmail.com>
2014-04-12 17:54:04 +0100

    static table structure drawn
    basic data generator
    
    table updates

---
+++ app/scripts/main.js
@@ -0,0 +1,102 @@
+/* global d3 */
+(function () {
+  'use strict';
+
+  function Tablamo (element, columns, data) {
+    this._element = element;
+    this._columns = columns;
+    this._data = data;
+
+    this.createTable(element);
+    this.bindDataToTableHeader(element, columns);
+    this.bindDataToTableBody(element, columns, data);
+  }
+
+  Tablamo.prototype.createTable = function (element) {
+    var table = d3.select(element).append('table');
+    table.append('thead').append('tr');
+    table.append('tbody');
+  };
+
+  Tablamo.prototype.bindDataToTableHeader = function (element, columns) {
+    var th = d3.select(element).select('thead tr').selectAll('th').data(columns);
+
+    th.html(function (d) {
+      return d.field;
+    });
+
+    th.enter().append('th').html(function (d) {
+      return d.field;
+    });
+
+    th.exit().remove();
+  };
+
+  Tablamo.prototype.bindDataToTableBody = function (element, columns, data) {
+    var tr = d3.select(element).select('tbody').selectAll('tr').data(data);
+
+    tr.enter().append('tr');
+
+    tr.exit().remove();
+
+    var td = tr.selectAll('td').data(function (d) {
+      var row = [];
+
+      columns.forEach(function (column, i) {
+        var cell = {};
+        cell.value = d[column.field];
+        row[i] = cell;
+      });
+
+      return row;
+    });
+
+    td.html(function (d) {
+      return d.value;
+    });
+
+    td.enter().append('td').html(function (d) {
+      return d.value;
+    });
+
+    td.exit().remove();
+  };
+
+  Tablamo.prototype.setColumns = function (columns) {
+    this._columns = columns;
+    this.bindDataToTableHeader(this._element, columns);
+  };
+
+  Tablamo.prototype.setData = function (data) {
+    this._data = data;
+    this.bindDataToTableBody(this._element, this._columns, data);
+  };
+
+  function generateData(columns) {
+    var data = [];
+
+    var generateCell = function (column) {
+      row[column.field] = (column.field === 'id') ? i : Math.random();
+    };
+
+    for (var i = 0; i < 100; i++) {
+      var row = {};
+
+      columns.forEach(generateCell);
+
+      data[i] = row;
+    }
+
+    return data;
+  }
+
+  var columns = [{field: 'id'}, {field: 'one'}, {field: 'two'}, {field: 'three'}];
+  var data = generateData(columns);
+
+  var tablamo = new Tablamo(document.body, columns, data);
+  
+  setInterval(function () {
+    tablamo.setData(generateData(columns));
+  }, 1000);
+
+})();
